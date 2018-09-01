239be006972c403f9d1c0c90b5700f5b1cf501f6
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-09-01 16:09:10 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,13 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
   validates :released, inclusion: { in: [true,false] }
+  validate :release_year_is_optional
   validates :title, uniqueness: { scope: :release_year}
-  validates :artist_name, presence: true 
-  
+  validates :artist_name, presence: true
+
+  def release_year_is_optional
+    if (released && ( release_year.nil? || release_year.empty? ) && release_year <= Date.today.year )
+      errors.add(:release_year, "Release year has to be present and not higher than current year!")
+    end 
+  end  
 end 
