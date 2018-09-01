da8f2b41a82233af9004a8b52f7948755b205065
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-09-01 16:54:16 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,23 @@
 class SongsController < ApplicationController
   def index 
-    
+    @songs = Song.all
+  end
+
+  def destroy 
+
   end 
+
+  def show 
+
+  end 
+
+  def edit 
+
+  end 
+  def new 
+
+  end 
+  def update 
+
+  end  
 end 
