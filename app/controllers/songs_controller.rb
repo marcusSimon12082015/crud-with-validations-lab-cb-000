c489a7013cabb977fcb3f0b2df3dc25b4fbcd3c7
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-09-01 17:11:58 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,7 +4,8 @@ class SongsController < ApplicationController
   end
 
   def destroy 
-
+    Song.find(params[:id]).destroy
+    redirect_to song_url
   end 
 
   def show 
@@ -13,13 +14,18 @@ class SongsController < ApplicationController
 
   def edit 
     @song = Song.find(params[:id])
-  end 
-  def new 
-
-  end 
-  def update 
-
   end
+   
+  def new 
+    @song = Song.new
+  end
+   
+  def update 
+    @song = Song.new(params.require(:song).permit(:title,:released,:release_year,:artist_name))
+    @song.save
+    redirect_to song_path(@song)
+  end
+  
   def create 
     @song = Song.new(params.require(:song).permit(:title,:released,:release_year,:artist_name))
     @song.save
