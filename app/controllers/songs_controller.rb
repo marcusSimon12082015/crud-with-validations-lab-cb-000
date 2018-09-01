126adb69e2c7323afa24d5055ec580072f5752c5
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-09-01 17:44:17 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,42 @@
+class SongsController < ApplicationController
+  def index 
+    @songs = Song.all
+  end
+
+  def destroy 
+    Song.find(params[:id]).destroy
+    redirect_to songs_url
+  end 
+
+  def show 
+    @song = Song.find(params[:id])
+  end 
+
+  def edit 
+    @song = Song.find(params[:id])
+  end
+   
+  def new 
+    @song = Song.new
+  end
+   
+  def update 
+    @song = Song.find(params[:id])
+    @song.update(params.require(:song).permit(:title,:released,:release_year,:artist_name))
+    if @song.valid?
+      redirect_to song_path(@song)
+    else 
+      render "edit"
+    end   
+  end
+  
+  def create 
+    @song = Song.new(params.require(:song).permit(:title,:released,:release_year,:artist_name))
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render "new"
+    end
+  end   
+end 
---
+++ app/models/song.rb
@@ -0,0 +1,18 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :released, inclusion: { in: [true,false] }
+  validate :release_year_is_optional
+  validates :title, uniqueness: { scope: :release_year, message: "should happen only once a year" }
+  validates :artist_name, presence: true
+
+  def release_year_is_optional
+    current_year = Date.current.year
+    puts current_year
+    puts attributes.inspect
+    if (released && release_year.nil?)
+      errors.add(:release_year, "Release year has to be present!")
+    elsif !release_year.nil? && release_year > current_year
+        errors.add(:release_year, "The release year cant be greater than the current year.")
+    end
+  end  
+end 
