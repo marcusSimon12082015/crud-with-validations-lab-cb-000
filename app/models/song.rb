f93652a6ed6e858ac1efbeb6a141bd9f500ada05
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-09-01 16:01:17 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,7 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
+  validates :released, inclusion: { in: [true,false] }
+  validates :title, uniqueness: { scope: :release_year}
+  validates :artist_name, presence: true 
+  
 end 
